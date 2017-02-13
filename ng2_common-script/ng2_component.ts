ad5e6b7624533c9b23efdacf3c4cbea2ca180277
mlWebDeveloper <mlwebdeveloper@outlook.com>
2017-02-12 21:27:48 -0500

    Update ng2_component.ts

--- ng2_common-script/ng2_component.ts
+++ ng2_common-script/ng2_component.ts
@@ -1,11 +1,13 @@
 // Common Angular 2 component sample
 import {Component} from '@angular/core';
+import {HelloWorld} from 'ng2-components/components';
 
-// import {HelloWorld} from 'ng2-components/components';
 @Component({
   moduleId: module.id,
   selector: 'ng-app',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.css'],
   providers: [HelloWorld]
-});
+})
+export class AppComponent {
+}
