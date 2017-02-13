2f374f7ee8869a63ac0539f7f82b0eaff9c9f51c
Marvin <manwize@live.com>
2017-02-12 21:10:56 -0500

    Angular 2 common import scripts
    Angular 2 common import scripts

---
+++ ng2_common-script/ng2_imports.ts
@@ -0,0 +1,10 @@
+// Common Angular 2 imports
+
+// Common Angular2 and TypeScript code
+import { Component } from '@angular/core';
+import { Http, HTTP_PROVIDERS } from 'angular2/http';
+import { Observable } from 'rxjs/observable';
+import { Injectable } from 'angular2/core'; // in a service
+import 'rxjs/Rx';
+import 'rxjs/add/operator/map';
+
