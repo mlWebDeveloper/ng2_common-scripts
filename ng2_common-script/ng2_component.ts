234b2fef659ab5136227a6bf050e8bfa67b32a5f
Marvin <manwize@live.com>
2017-02-12 21:20:39 -0500

    Angular 2 sample component
    Angular 2 sample component

---
+++ ng2_common-script/ng2_component.ts
@@ -0,0 +1,13 @@
+// Common Angular 2 imports
+
+// Common Angular2 and TypeScript code
+import {Component} from '@angular/core';
+// import {HelloWorld} from 'ng2-components/components';
+ 
+@Component({
+  moduleId: module.id,
+  selector: 'ng-app',
+  templateUrl: 'app.component.html',
+  styleUrls: ['app.component.css'],
+  providers: [AppComponent]
+});
